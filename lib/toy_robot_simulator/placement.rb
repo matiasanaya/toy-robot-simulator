a3ee2530d390245759e4c4113773b6b5738cd9f5
Matias Anaya <matiasanaya@gmail.com>
2014-09-07 17:05:18 +1000

    Rewrite Placement to pass PlacementTest
    PlacementTest had been modified and it specified a new public
    interface.
    
    Placement had to be rewritten. In the process it was simplified.

--- lib/toy_robot_simulator/placement.rb
+++ lib/toy_robot_simulator/placement.rb
@@ -3,27 +3,21 @@ require 'forwardable'
 class Placement
   extend Forwardable
 
-  def_delegators :pose, :rotate!, :report
+  def_delegators :pose, :rotate, :report
 
   def initialize(args)
     @board = args[:board]
     @pose = args[:pose]
   end
 
-  def update!(a_new_pose)
+  def update(a_new_pose)
     if board.valid_pose?(a_new_pose)
       self.pose = a_new_pose
     end
-    pose
   end
 
-  def adjacent
-    adjacent_pose = pose.adjacent
-    if board.valid_pose?(adjacent_pose)
-      self.class.new(board: board, pose: adjacent_pose)
-    else
-      self
-    end
+  def advance
+    update(pose.adjacent)
   end
 
   private
