97cd0482d25b2a41af69521e70e81daf2981370a
Matias Anaya <matiasanaya@gmail.com>
2014-09-07 18:52:52 +1000

    Improve style of Pose
    Added whitespace to array literals. Moved Pose::Orientation
    constants into a module declaration.

--- lib/toy_robot_simulator/pose.rb
+++ lib/toy_robot_simulator/pose.rb
@@ -1,9 +1,10 @@
 class Pose
-  Pose::Orientation = Module.new
-  Pose::Orientation::EAST = :east
-  Pose::Orientation::NORTH = :north
-  Pose::Orientation::WEST = :west
-  Pose::Orientation::SOUTH = :south
+  module Orientation
+    EAST = :east
+    NORTH = :north
+    WEST = :west
+    SOUTH = :south
+  end
 
   def initialize(args)
     @x = args[:x]
@@ -12,13 +13,12 @@ class Pose
   end
 
   def adjacent
-    adjacent_pose = dup.send(:adjacent!)
+    dup.send(:adjacent!)
   end
 
   def rotate!(degrees)
     step = (degrees % 90) + (degrees/degrees.abs)
     self.orientation = step_orientation(step)
-    self
   end
 
   def report
@@ -35,11 +35,11 @@ class Pose
 
   def step_orientation(by = 1)
     orientations = [
-                    Pose::Orientation::NORTH,
-                    Pose::Orientation::EAST,
-                    Pose::Orientation::SOUTH,
-                    Pose::Orientation::WEST
-                  ]
+      Pose::Orientation::NORTH,
+      Pose::Orientation::EAST,
+      Pose::Orientation::SOUTH,
+      Pose::Orientation::WEST
+    ]
 
     orientations[(orientations.index(orientation) + by) % 4]
   end
@@ -69,9 +69,9 @@ class Pose
   def update!(coordinate, by)
     case coordinate
     when :x
-      @x += by
+      self.x = x + by
     when :y
-      @y += by
+      self.y = y + by
     end
   end
 end
