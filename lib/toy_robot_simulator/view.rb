5b0b5c3fe551b2fd0f479d7efeed6bd925f4d78f
Matias Anaya <matiasanaya@gmail.com>
2014-09-09 08:47:49 +1000

    Implement View according to ViewTest

---
+++ lib/toy_robot_simulator/view.rb
@@ -0,0 +1,20 @@
+View = Struct.new(:args) do
+  def report
+    report = robot.report
+    output.puts report_string(report) if report
+  end
+
+  private
+
+  def report_string(report)
+    "(#{report[:x]},#{report[:y]}) #{report[:orientation].upcase}"
+  end
+
+  def robot
+    args[:robot]
+  end
+
+  def output
+    args[:output] || $stdout
+  end
+end
