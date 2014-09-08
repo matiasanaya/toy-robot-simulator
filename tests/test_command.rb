20967df1805b1c96f0476d691021ddc8621d4df7
Matias Anaya <matiasanaya@gmail.com>
2014-09-08 14:38:25 +1000

    Add tests for Command::Token::* (PLACE, MOVE, etc)
    And also test for Command::Base

---
+++ tests/test_command.rb
@@ -0,0 +1,20 @@
+require './lib/toy_robot_simulator/command/command'
+require 'minitest/autorun'
+
+class CommandTest < MiniTest::Unit::TestCase
+  def test_tokens_are_defined
+    assert defined?(Command::Token::PLACE)
+
+    assert defined?(Command::Token::MOVE)
+
+    assert defined?(Command::Token::RIGHT)
+
+    assert defined?(Command::Token::LEFT)
+
+    assert defined?(Command::Token::REPORT)
+  end
+
+  def test_base_is_defined
+    assert defined?(Command::Base)
+  end
+end
