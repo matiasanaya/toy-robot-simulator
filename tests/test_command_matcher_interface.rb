2ae6c5a4852631f7b64a26f2cf4c5fac8705f35f
Matias Anaya <matiasanaya@gmail.com>
2014-09-08 12:20:35 +1000

    Refactor general matcher behavior out of CommandMatcherMoveTest
    Now all matchers can be tested for general interface and
    behavior by including CommandMatcherInterfaceTest.
    
    Tests for Command::Matcher::Left were added in this commit.

---
+++ tests/test_command_matcher_interface.rb
@@ -0,0 +1,29 @@
+module CommandMatcherInterfaceTest
+  def test_it_implement_the_command_matcher_interface
+    assert_respond_to @object, :build_with_match
+  end
+
+  def test_that_it_matches_a_valid_string
+    assert @object.build_with_match(@object_valid_str)
+  end
+
+  def test_that_it_ignores_invalid_strings
+    @object_invalid_strs.each do |invalid|
+      refute @object.build_with_match(invalid), "It should ignore '#{invalid}'"
+    end
+  end
+
+  def test_that_response_knows_token
+    assert_equal @object_token,
+                 @object.build_with_match(@object_valid_str).token
+  end
+
+  def test_that_response_knows_args
+    assert_equal @object.send(:extract_args, @object_valid_str),
+                 @object.build_with_match(@object_valid_str).args
+  end
+
+  def test_that_response_is_nil
+    assert_equal nil, @object.build_with_match(@object_invalid_strs.sample)
+  end
+end
--- tests/test_command_matcher_left.rb
+++ tests/test_command_matcher_left.rb
@@ -2,18 +2,17 @@ require './lib/toy_robot_simulator/command/matcher/base'
 require './tests/test_command_matcher_interface'
 require 'minitest/autorun'
 
-class CommandMatcherMoveTest < MiniTest::Unit::TestCase
+class CommandMatcherLeftTest < MiniTest::Unit::TestCase
   include CommandMatcherInterfaceTest
 
   def setup
-    @object = Command::Matcher::Move
-    @object_token = Command::Token::MOVE
-    @object_valid_str = 'MOVE'
+    @object = Command::Matcher::Left
+    @object_token = Command::Token::LEFT
+    @object_valid_str = 'LEFT'
     @object_invalid_strs = [
-      'MOVE ',
-      'MOVER',
-      'MOVE 180',
-      'move'
+      'LEFT ',
+      'LEFTY',
+      'LEFT 180'
     ]
   end
 end
