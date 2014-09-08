e1a64a95d51e6976c49a24de217e41cf6ad0e045
Matias Anaya <matiasanaya@gmail.com>
2014-09-08 14:25:56 +1000

    Rewrite RobotControllerTest to test for delegation pattern.
    New tests document the fact that the controller is going to delegate
    most inputs to either the robot or the view.

--- tests/test_robot_controller.rb
+++ tests/test_robot_controller.rb
@@ -2,19 +2,59 @@ require './lib/toy_robot_simulator/robot_controller'
 require 'minitest/autorun'
 
 class RobotControllerTest < MiniTest::Unit::TestCase
+  CommandDouble = Struct.new(:token, :args)
+
   def setup
+    @command_double = CommandDouble.new(nil, nil)
     @robot = MiniTest::Mock.new
-    @robot_controller = RobotController.new(robot: @robot)
+    @view = MiniTest::Mock.new
+    @robot_controller = RobotController.new(
+      robot: @robot,
+      view: @view
+    )
   end
 
   def test_respond_to_public_interface
-    assert_respond_to @robot_controller, :execute
+    assert_respond_to @robot_controller, :input
   end
 
-  def test_it_executes_a_place_command
-    command = 'PLACE 0,0,NORTH'
-    @robot.expect(:place, nil, ['some_args'])
-    @robot_controller.execute('some_args')
+  def test_it_delegates_a_place_command_to_robot
+    @command_double.token = Command::Token::PLACE
+
+    @robot.expect(:place, nil, [@command_double.args])
+    @robot_controller.input(@command_double)
     @robot.verify
+  end
+
+  def test_it_delegates_a_move_command_to_robot
+    @command_double.token = Command::Token::MOVE
+
+    @robot.expect(:move, nil)
+    @robot_controller.input(@command_double)
+    @robot.verify
+  end
+
+  def test_it_delegates_a_left_command_to_robot
+    @command_double.token = Command::Token::LEFT
+
+    @robot.expect(:left, nil)
+    @robot_controller.input(@command_double)
+    @robot.verify
+  end
+
+  def test_it_delegates_a_right_command_to_robot
+    @command_double.token = Command::Token::RIGHT
+
+    @robot.expect(:right, nil)
+    @robot_controller.input(@command_double)
+    @robot.verify
+  end
+
+  def test_it_delegates_a_report_command_to_view
+    @command_double.token = Command::Token::REPORT
+
+    @view.expect(:report, nil)
+    @robot_controller.input(@command_double)
+    @view.verify
   end
 end
