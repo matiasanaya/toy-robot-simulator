71bc2e61c819821114e9e38a9a6a1761f9e425f6
Matias Anaya <matiasanaya@gmail.com>
2014-09-08 14:28:15 +1000

    Implement RobotController complaint with RobotControllerTest

--- lib/toy_robot_simulator/robot_controller.rb
+++ lib/toy_robot_simulator/robot_controller.rb
@@ -1,7 +1,33 @@
+require './lib/toy_robot_simulator/command/command'
+require 'forwardable'
+
 class RobotController
-  def initialize(_)
+  extend Forwardable
+
+  def_delegators :robot, :place, :move, :left, :right
+  def_delegator :view, :report
+
+  def initialize(args)
+    @robot = args[:robot]
+    @view = args[:view]
   end
 
-  def execute(_)
+  def input(command)
+    case command.token
+    when Command::Token::PLACE
+      place(command.args)
+    when Command::Token::MOVE
+      move
+    when Command::Token::RIGHT
+      right
+    when Command::Token::LEFT
+      left
+    when Command::Token::REPORT
+      report
+    end
   end
+
+  private
+
+  attr_reader :robot, :view
 end
