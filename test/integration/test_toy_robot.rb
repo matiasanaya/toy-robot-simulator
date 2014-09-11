188d23f86df45fd77df16f80e12f9a71436cf9dc
Matias Anaya <matiasanaya@gmail.com>
2014-09-11 22:01:13 +1000

    Fix executable assertion in ToyRobotTest

--- test/integration/test_toy_robot.rb
+++ test/integration/test_toy_robot.rb
@@ -3,7 +3,7 @@ require 'minitest/autorun'
 module ToyRobot
   class ToyRobotTest < MiniTest::Unit::TestCase
     def test_that_it_is_executable
-      assert `bin/toyrobot < data/example_input_a.txt`
+      assert system('[ -x bin/toyrobots ]')
     end
   end
 end
