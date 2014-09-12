111da38a55c7db0cb353cf173a7f177e57501c5f
Matias Anaya <matiasanaya@gmail.com>
2014-09-12 11:27:49 +1000

    Remove private method debug() from ToyRobot::Application
    This method was untested and undocumented. It is of doubtful
    value and thus has been removed from the code base.

--- lib/toy_robot/application.rb
+++ lib/toy_robot/application.rb
@@ -14,7 +14,6 @@ module ToyRobot
         raw_input = input.gets
         break unless raw_input
         raw_input.chomp!
-        debug(raw_input)
         command = parser.parse(raw_input)
         controller.send(command.token, command.args) if command
       end
@@ -23,16 +22,5 @@ module ToyRobot
     private
 
     attr_reader :input, :parser, :controller
-
-    def debug(raw_input)
-      case raw_input
-      when /\AR(EPORT_DEBUG)*\z/
-        puts controller.instance_variable_get(:@robot).inspect
-      when /\AE(XIT)*\z/
-        puts 'Have a nice day, bye :)'
-        puts '...'
-        exit(false)
-      end
-    end
   end
 end
