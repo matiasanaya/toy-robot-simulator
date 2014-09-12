5c135707083a37562b53ede9c1c0eb12d7892634
Matias Anaya <matiasanaya@gmail.com>
2014-09-12 10:10:39 +1000

    Fix filename typo in failling ToyRobotTest

--- test/integration/test_toy_robot.rb
+++ test/integration/test_toy_robot.rb
@@ -3,7 +3,7 @@ require 'minitest/autorun'
 module ToyRobot
   class ToyRobotTest < MiniTest::Unit::TestCase
     def test_that_it_is_executable
-      assert system('[ -x bin/toyrobots ]')
+      assert system('[ -x bin/toyrobot ]')
     end
   end
 end
