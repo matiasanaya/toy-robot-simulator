2123b971bb06da8efa01e2432ea8e7ed4d9ff66f
Matias Anaya <matiasanaya@gmail.com>
2014-09-15 12:51:14 +1000

    Remove PoserDouble class and tests
    These are no longer needed, since a hash can play that role.

--- test/poser_double.rb
+++
@@ -1,9 +0,0 @@
-class PoserDouble
-  attr_reader :x, :y, :orientation
-
-  def initialize(args = {})
-    @x = args[:x]
-    @y = args[:y]
-    @orientation = args[:orientation]
-  end
-end
--- test/test_poser_interface.rb
+++
@@ -1,9 +0,0 @@
-module ToyRobot
-  module PoserInterfaceTest
-    def test_it_implement_the_poser_interface
-      assert_respond_to @object, :x
-      assert_respond_to @object, :y
-      assert_respond_to @object, :orientation
-    end
-  end
-end
